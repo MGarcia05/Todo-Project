436e141843e5ce9c86638adcfc760483b9e47ff0
Melissa Garcia <megarciade@outlook.com>
2022-03-25 17:27:38 -0400

    Todo Project with Dev Ed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+/* Heavily inspired by Dev Ed's video on Youtube,
+  Build A Todo App With REACT | React Project For Beginners (FULL).
+  Learned a lot, what a great resource! 
+*/
+
+import React, { useState,useEffect } from 'react';
 import './App.css';
+//Import Components
+import Form from "./components/Form.js";
+import TodoList from "./components/TodoList.js";
+import Todo from "./components/Todo.js"
 
 function App() {
+  //states
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status,setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // run once when the app starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  //use effect
+  useEffect(() => {
+    saveLocalTodos();
+    filterHandler();
+  }, [todos, status]);
+
+
+  //functions and events
+
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed == true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed == false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  //save to local storage 
+  const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem("todos") ===null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+    setTodos(todoLocal);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>Todo List</h1>
+    </header>
+    <Form 
+    inputText={inputText}
+    todos={todos} 
+    setTodos={setTodos} 
+    setInputText={setInputText}
+    setStatus={setStatus}
+    />
+    <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
-}
+} 
 
 export default App;
